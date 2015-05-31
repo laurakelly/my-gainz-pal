5b6063e686e6f5eec80547afbe56389ff04e0e22
Laura Kelly <laurakelly135@gmail.com>
2015-05-31 02:39:47 -0700

    Load test data; Add jQuery and D3

--- src/js/components/MainSection.js
+++ src/js/components/MainSection.js
@@ -1,7 +1,18 @@
 var React = require('react');
+var $ = require('jquery');
+var d3 = require('d3');
 
 var MainSection = React.createClass({
+  getInitialState: function () {
+    return {data: null}
+  },
+  componentDidMount: function() {
+    $.getJSON('src/test_data/mfp-export-1431415257924.json', function(data) {
+      this.setState({'data': data});
+    }.bind(this))    
+  },
   render: function() {
+    console.log(this.state.data);
     return (
       <h1>MainSection</h1>
     )
