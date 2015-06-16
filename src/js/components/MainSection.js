134e7271321f21bbe79d8a114419382a9aacec80
Laura Kelly <laurakelly135@gmail.com>
2015-06-16 16:55:17 -0700

    Add basic scatterplot; Clean data; Add Underscore

--- src/js/components/MainSection.js
+++ src/js/components/MainSection.js
@@ -1,21 +1,32 @@
-var React = require('react');
-var $ = require('jquery');
-var Scatterplot = require('./Scatterplot');
+var React = require("react");
+var $ = require("jquery");
+var _ = require("underscore")
+var Scatterplot = require("./Scatterplot");
+
+/* Graph defaults */
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+  width = 960 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
 
 var MainSection = React.createClass({
   getInitialState: function () {
     return {data: null}
   },
   componentDidMount: function() {
-    $.getJSON('src/test_data/mfp-export-1431415257924.json', function(data) {
-      this.setState({'data': data});
+    $.getJSON("src/test_data/mfp-export-1434497438002.json", function(data) {
+      var cleaned = _.filter(data, function(d) { if (d.weight !== "") return true})
+      this.setState({"data": cleaned});
     }.bind(this))    
   },
   render: function() {
-    console.log(this.state.data);
     return (
-      <div className='mainSection'>
-        <Scatterplot data={this.state.data} />
+      <div className="mainSection">
+        <Scatterplot data={this.state.data}
+          margin={margin}
+          height={height}
+          width={width}
+          xProperty={"ts"}
+          yProperty={"weight"} />
       </div>
     )
   }
--- src/js/components/Scatterplot.js
+++ src/js/components/Scatterplot.js
@@ -1,10 +1,88 @@
-var React = require('react');
-var d3 = require('d3');
+var React = require("react");
+var d3 = require("d3");
+var _ = require("underscore");
+
+function minMax(data, property) {
+  var getX = function (d) { return d[property]};
+
+  return[_.min(data, getX), _.max(data, getX)].map(getX);
+}
+
+// TODO set min/max in state so it doesn't have to be recalculated unnecessarily
 
 var Scatterplot = React.createClass({
+  componentDidMount: function() {
+    console.log(this.props);
+    d3.select(".scatterplot").append("svg")
+        .attr("width", this.props.width + this.props.margin.left + 
+          this.props.margin.right)
+        .attr("height", this.props.height + this.props.margin.top + 
+          this.props.margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + this.props.margin.left + "," +
+          this.props.margin.top + ")");
+  },
+  componentDidUpdate: function() {
+    var svg = d3.select(".scatterplot").select("svg");
+
+    var x = d3.scale.linear()
+        .range([0, this.props.width])
+        .domain(minMax(this.props.data, this.props.xProperty));
+
+    var  y = d3.scale.linear()
+        .range([this.props.height, 0])
+        .domain(minMax(this.props.data, this.props.yProperty));
+
+    var color = d3.scale.category10();
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + this.props.height + ")")
+      .call(xAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("x", this.props.width)
+      .attr("y", -6)
+      .style("text-anchor", "end")
+      .text("Date");
+
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("lb");
+
+    // maybe this should be stored somewhere?
+    var xProperty = this.props.xProperty,
+      yProperty = this.props.yProperty;
+
+    console.log(minMax(this.props.data, this.props.yProperty))
+
+    svg.selectAll(".dot")
+       .data(this.props.data)
+      .enter().append("circle")
+        .attr("class", "dot")
+        .attr("r", 3.5)
+        .attr("cx", function(d){ 
+          return x(Number(d[xProperty])); })
+        .attr("cy", function(d){ return y(Number(d[yProperty])); })
+        .style("fill", "purple");
+  },
   render: function() {
-    console.log(this.props.data);
-    return (<p>{this.props.data}</p>);
+    return (<div className="scatterplot"></div>);
   }
 });
 
